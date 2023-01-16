685ae8f12a7f2373a78c04c8ca7e3f924bc29ec6
Alexander Z <alexber7@mail.ru>
2023-01-16 18:24:17 +0300

    Добавлены Задачи 1-4 по первому семинару

---
+++ HomeWork/HW_1/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Находим макисмум из двух введенных чисел");
+Console.WriteLine("Пожалуйста, введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int maxnumber = Math.Max(number1, number2);
+Console.WriteLine("Максимальное число "+maxnumber);
---
+++ HomeWork/HW_1/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает три числа и выдаёт, какое число большее, а какое меньшее.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Находим макисмум из трех введенных чисел");
+Console.WriteLine("Пожалуйста, введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите третье число ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int maxnumber = 0;
+for (int i = 1; i<3; i++)
+{
+    if (maxnumber < number1)
+        maxnumber = number1;
+    if (maxnumber < number2)
+        maxnumber = number2;
+    if (maxnumber < number3)
+        maxnumber = number3;
+}
+
+Console.WriteLine("Максимальное число "+maxnumber);
