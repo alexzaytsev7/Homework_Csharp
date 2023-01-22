96ff91ee2448f05b4fe196dbd06a82f74b9ec64b
Alexander Z <alexber7@mail.ru>
2023-01-22 13:58:50 +0300

    add HW_3

---
+++ HomeWork/HW_3/Task1/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно 
+// палиндромом (НЕ использовать число как строку, то есть сравнения типа number[0] == number[4] 
+// делать НЕЛЬЗЯ. Используем операторы % и /).
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+//  int Palindromnumber(int number) {
+
+//    if()
+// }
+// int a1, a2, a3, a4, a5 = 0;
+// int b1, b2, b3, b4, b5 = 0;
+
+
+int c = 4; //коли-во цифр в числе -1
+int i = 0;
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+double result = Convert.ToDouble(number);
+double b5 = result;
+double b6 = 0;
+double b7 = result;
+   
+if (number < 9999 || number > 99999){
+        Console.WriteLine("Введено не пятизначное число");
+}
+
+while (c>=0){
+   int mnogitel = Convert.ToInt32(Math.Pow(10, i));
+
+    int delitel = Convert.ToInt32(Math.Pow(10, c));
+    
+    b5 = Math.Round(b7 / mnogitel  % 10, MidpointRounding.ToZero);
+    b6 = b6 + b5 * delitel;
+
+    i++;
+    c=c-1;
+}
+
+Console.WriteLine("Введенное число в обратном порядке: "+b6);
+
+// while (c>=0){
+//    int mnogitel = Convert.ToInt32(Math.Pow(10, i));
+
+//     int delitel = Convert.ToInt32(Math.Pow(10, c));
+    
+//     b5 = Math.Round(b7 / mnogitel  % 10, MidpointRounding.ToZero);
+//     b6 = b6 + b5 * delitel;
+
+//     i++;
+//     c=c-1;
+// }
+double a1, a2, a4, a5;
+
+a1 = Math.Round(b7 / 1  % 10, MidpointRounding.ToZero);
+a2 = Math.Round(b7 / 10  % 10, MidpointRounding.ToZero);
+a4 = Math.Round(b7 / 1000  % 10, MidpointRounding.ToZero);
+a5 = Math.Round(b7 / 10000  % 10, MidpointRounding.ToZero);
+
+if (a1 == a5 && a2 == a4){
+    Console.WriteLine("Введенное число является палиндром");
+}
+else
+    Console.WriteLine("Введенное число НЕ является палиндром");
+
---
+++ HomeWork/HW_3/Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит 
+// расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int xa, ya, za, xb, yb, zb;
+
+Console.WriteLine("Введите координатy x А");
+if(!int.TryParse(Console.ReadLine(), out xa))
+    Console.WriteLine("Введено не целое число");
+    
+Console.WriteLine("Введите координатy y А");
+if(!int.TryParse(Console.ReadLine(), out ya))
+    Console.WriteLine("Введено не целое число");
+    
+Console.WriteLine("Введите координатy z А");
+if(!int.TryParse(Console.ReadLine(), out za))
+    Console.WriteLine("Введено не целое число");
+    
+
+    Console.WriteLine("Введите координатy x B");
+if(!int.TryParse(Console.ReadLine(), out xb))
+    Console.WriteLine("Введено не целое число");
+    
+Console.WriteLine("Введите координатy y B");
+if(!int.TryParse(Console.ReadLine(), out yb))
+    Console.WriteLine("Введено не целое число");
+    
+Console.WriteLine("Введите координатy z B");
+if(!int.TryParse(Console.ReadLine(), out zb))
+    Console.WriteLine("Введено не целое число");
+    
+
+double Distance(double xa, double ya, double za, double xb, double yb, double zb)
+{
+    return Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2)+ Math.Pow((zb - za), 2));    
+}
+
+double res = Distance(xa,ya,za,xb,yb,zb);
+Console.WriteLine($"Расстоняние между тремя точками: {res.ToString("0.##")}");
