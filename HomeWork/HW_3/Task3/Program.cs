30a48e2e4d31489c967ede7fcca11ee05a73a063
Alexander Z <alexber7@mail.ru>
2023-01-22 16:01:15 +0300

    add HW_3 task 1

---
+++ HomeWork/HW_3/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int number, x;
+Console.WriteLine("Введите число от 1 до N");
+
+if(!int.TryParse(Console.ReadLine(), out number)){
+        Console.WriteLine("Введено не целое число");
+        return;
+}
+
+if(number < 0){
+    Console.WriteLine("Введено отрицательное число");
+    return;
+}
+
+
+double Cub(double x)
+{
+    return (x * x * x);
+}
+
+double res = Cub(number);
+Console.WriteLine($"Куб введенного числа: {res}");
