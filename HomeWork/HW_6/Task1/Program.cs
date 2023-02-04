e3875f30cfd40f38f2c5c8e280679d4a32eb3df8
Alexander Z <alexber7@mail.ru>
2023-02-04 16:08:41 +0300

    add HW_6

---
+++ HomeWork/HW_6/Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.WriteLine("Введите размерность массива:");
+            int i, n = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[n];
+            Console.WriteLine("Введите массив:");
+            string[] str = Console.ReadLine().Split(new char[] { ' ',',', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+void PrintArray(int [] mas)
+{
+            for (i = 0; i < (n < str.Length ? n : str.Length); ++i){
+                mas[i] = Convert.ToInt32(str[i]);
+            }
+                  
+
+}
+
+
+void CountPolozitelnyh(int [] mas)
+{
+    int result = 0;   
+    for(i=0; i < n ; i++){
+        if(mas[i] > 0){
+        result++;    
+        }
+    
+    }
+    Console.WriteLine($"Кол-во чисел в масиве больше 0: {result}");
+}
+
+//Проверка на указанное кол-во элемнтов массива с реально введенным
+
+PrintArray(mas);
+if (str.Length != n)
+{
+    Console.WriteLine("Указанное кол-во элемнтов массива не совпадает с реально введенным!");
+return;
+}
+else
+CountPolozitelnyh(mas);
---
+++ HomeWork/HW_6/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+           
+Console.WriteLine("Введите значения b1, k1, b2 и k2:");
+double[] mas = new double[4];
+string[] str = Console.ReadLine().Split(new char[] { ' ',',', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+void PrintArray(double [] mas)
+{
+            for (int i = 0; i < 4; ++i){
+                mas[i] = Convert.ToDouble(str[i]);
+            }               
+}
+  
+
+float[] CrossPoint(double [] mas)
+{
+  double x, y;
+
+  float [] arrNum = new float[2];
+  
+  x = (mas[2] - mas[0]) / (mas[1] - mas[3]);      
+  y = mas[1] * x + mas[0];
+
+  Console.WriteLine($"Точка пересечения двух прямых: ({x}; {y})");   
+  return arrNum;           
+}
+
+PrintArray(mas);
+
+ if(mas[0] == mas[2]){
+        Console.WriteLine("Отрезки параллельны");
+        return;
+    }
+
+CrossPoint(mas);
