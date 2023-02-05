dbbd0d60413199155c94b55c9d9623a09f7f6090
Alexander Z <alexber7@mail.ru>
2023-02-05 12:41:51 +0300

    оптимизировал ввод массива из клавиатуры

---
+++ HomeWork/HW_5/Task2/var2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите массив:");
+int i = 0;   
+int[] mas = Console.ReadLine().Split( ' ',',', '\n', '\t' ).Select(Int32.Parse).ToArray();
+int n = mas.Length;  
+
+void SumNaNechetPoz(int [] mas)
+{
+    int result = 0;   
+    for(i=0; i < n ; i+=2){
+    result +=mas[i];
+    }
+    Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {result}");
+}
+
+SumNaNechetPoz(mas);
